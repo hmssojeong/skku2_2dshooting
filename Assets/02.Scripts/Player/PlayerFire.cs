3d0553fd75e15fc4e5c8626199f63a78ba462f2b
hmssojeong <sojeong9638@gmail.com>
2025-11-04 15:46:15 +0900

    1104 총알 발사에 쿨타임 적용

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -10,44 +10,39 @@ public class PlayerFire : MonoBehaviour
 
     [Header("총구")]
     public Transform FirePosition;
-
     public float FireOffset = 0.3f;
-    public float FireCoolTime = 0.6f;
-    public float FireTime = 0;
 
-    // 자동 공격 모드 여부
-    public bool AutoFire = true;
+    [Header("쿨타임")]
+    public const float CoolTime = 0.6f;
+    private float _coolTimer;
+
 
     private void Update()
     {
-        // 1. 발사 버튼을 누르고 있으면..
-        if(Input.GetKey(KeyCode.Space) && Time.time >= FireTime)
-        {
-            if(Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                AutoFire = true;
-            }
-            else if(Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                AutoFire = false;
-            }
+        _coolTimer -= Time.deltaTime;
+        if (_coolTimer > 0) return;     // 조기 리턴
 
-            // 다음 발사 시간을 지금 시간 + 쿨타임으로 설정
-            FireTime = Time.time + FireCoolTime;
-            // -> Time.time: 게임 시작 후 경과된 총 시간입니다.
+        // 1. 발사 버튼을 누르고 있으면..
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // 발사하고 나면 쿨타이머를 초기화
+            _coolTimer = CoolTime;
 
             // 유니티에서 게임 오브젝트를 생성할때는 new가 instaintate 라는 메서드를 이용한다.
             // 클래스 -> 객체(속성+기능) -> 메모리에 로드된 객체를 인스턴스
-            //                           ㄴ 인스턴스화
+            //                        ㄴ 인스턴스화
 
-            //GameObject bullet = new BulletPrefab(); -> GameObject bullet = Instantiate(BulletPrefab);
             // 2. 프리팹으로부터 총알(게임 오브젝트)을 생성한다.
             GameObject bullet1 = Instantiate(BulletPrefab);
             GameObject bullet2 = Instantiate(BulletPrefab);
 
-            // 3. 생성된 총알의 위치를 총구의 위치로 옮긴다.
-            bullet1.transform.position = FirePosition.position + new Vector3(-FireOffset, 0, 0); // 생성 후 총구의 위치로 수정
+            // 3. 총알의 위치를 총구 위치로 바꾸기 
+            bullet1.transform.position = FirePosition.position + new Vector3(-FireOffset, 0, 0);
             bullet2.transform.position = FirePosition.position + new Vector3(FireOffset, 0, 0);
         }
+
+
     }
+
+
 }
