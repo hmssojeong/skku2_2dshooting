3930062940a2d76f467fdbf35d0eb3ae2f8848c0
hmssojeong <sojeong9638@gmail.com>
2025-11-05 13:51:22 +0900

    1. 플레이어는 적과 3번 충돌하면 죽게 만들기 / 2. 몬스터 체력(100)이 0일때 몬스터 죽이기

--- Assets/02.Scripts/Bullet/Bullet.cs
+++ Assets/02.Scripts/Bullet/Bullet.cs
@@ -2,61 +2,57 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // 목표: 위로 계속 이동하고 싶다.
 
     // 필요 속성
-    [Header("이동속도")]
-    private float _speed;
-    public float StartSpeed = 1;
-    public float EndSpeed = 7;
+    [Header("이동")]
+    public float StartSpeed = 1f;
+    public float EndSpeed = 7f;
     public float Duration = 1.2f;
-    // Update is called once per frame
+    private float _speed;
+
+    [Header("공격력")]
+    public float Damage;
 
     private void Start()
     {
         _speed = StartSpeed;
     }
-    void Update()
+
+    private void Update()
     {
         // 목표: Duration 안에 EndSpeed까지 달성하고 싶다.
-        
-        
-        float Acceleration = (EndSpeed - StartSpeed) / Duration;
-        //                        6     /   1.2   =  5
-        _speed += Time.deltaTime * Acceleration;  // 초당 +  1 * 가속도
+
+        // 논리적인 실수(1), 코딩 컨벤션(1)
+        float acceleration = (EndSpeed - StartSpeed) / Duration;
+        //                     6      / 1.2   = 5
+        _speed += Time.deltaTime * acceleration;   // 초당 + 1 * 가속도
         _speed = Mathf.Min(_speed, EndSpeed);
         //         ㄴ 어떤 속성과 어떤 메서드를 가지고 있는지 톺아볼 필요가 있다.
 
 
-
         // 방향을 구한다.
-        //Vector2 direction = new Vector2(0, 1); //위쪽 방향
         Vector2 direction = Vector2.up;
 
-        // 방향에 따라 이동한다.
-        // 새로운 위치는 = 현재위치 + 방향 * 속도 * 시간  
+        // 공식에 따라 이동한다.
+        // 새로운 위치는 = 현재 위치 + 방향 * 속력 * 시간
         Vector2 position = transform.position;
         Vector2 newPosition = position + direction * _speed * Time.deltaTime;
         transform.position = newPosition;
-        
     }
 
-    public void OnTriggerEnter2D(Collider2D other)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         // 총알은 Enemy와만 충돌 이벤트를 처리한다.
         if (other.CompareTag("Enemy") == false) return;
 
-        // GameObject enemyGameObject = other.gameObject;
-        
-        // GetComponent는 게임오브젝트에 붙어있는 컴포넌트를 가져올 수 있다.
+        // GetComponent는 게임오브젝트에 붙어있는 컴포넌트를 가져올수있다. 
         Enemy enemy = other.gameObject.GetComponent<Enemy>();
 
-        Debug.Log(enemy.Health);
-        enemy.Health -= 60f;
-        if (enemy.Health <= 0)
+        // 객체간의 상호 작용을 할때 : 묻지말고 시켜라(디미터의 법칙)
+        enemy.Hit(Damage);
 
         Destroy(gameObject);
-        Destroy(other.gameObject);
-
     }
 }
--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -1,28 +1,37 @@
-using System.Transactions;
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [Header("스탯")]
     public float Speed;
-    public float Health = 100f;
+    public float Damage = 1;
+    private float _health = 100f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Vector2 direction = Vector2.down; // 아래 방향으로
-        transform.Translate(translation: direction * (Speed * Time.deltaTime));
+        Vector2 direction = Vector2.down;
+        transform.Translate(direction * (Speed * Time.deltaTime));
+    }
+
+    public void Hit(float damage)
+    {
+        _health -= damage;
+
+        if (_health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //몬스터는 플레이어와만 충돌처리할 것이다.
-        //if(other.gameObject.tag == "Player") //name으로 하면 누군가가 이름을 다른것으로 바꿀경우 문제가 생기기때문에 tag로 구분하는게 좋다.
+        // 몬스터는 플레이어와만 충돌처리할 것이다.
         if (!other.gameObject.CompareTag("Player")) return;
-        {
-            Destroy(gameObject);       // 나 죽고,
-            Destroy(other.gameObject); // 너 죽자 (플레이어, 총알)
-        }
-    }
 
+        Player player = other.gameObject.GetComponent<Player>();
+        player.Hit(Damage);
+
+        Destroy(other.gameObject);    // 나죽자.
+    }
 }
